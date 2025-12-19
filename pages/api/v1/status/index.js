7e3e0ba452fe13c42a19bbc07745502405e3fdd6
Marcus Costa <115756320+costamarcus@users.noreply.github.com>
2025-12-19 19:04:21 +0000

    make database.js more robust to error hamdling

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,39 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
+  // Date format ISO 8651
+  const updateAt = new Date().toISOString();
+
+  // Server info
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databeMaxConectionResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databeMaxConectionValue =
+    databeMaxConectionResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1 and state = 'active';",
+    values: [databaseName],
+  });
+
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
   response.status(200).json({
-    msg: "alunos do curso.dev são pessoal acima da média",
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        status: "healthy",
+        max_connections: parseInt(databeMaxConectionValue),
+        opened_connections: databaseOpenedConnectionsValue,
+        latency: {},
+        version: databaseVersionValue,
+      },
+    },
   });
 }
 
